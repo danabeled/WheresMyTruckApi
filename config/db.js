3da4513bb438b7207cb040d59a7b23618f810336
Daniel Abel <danabeled@gmail.com>
2019-04-13 19:48:28 -0400

    move repopulate to database

--- config/db.js
+++ config/db.js
@@ -3,16 +3,46 @@ const MongoClient = require('mongodb');
 const urlString = `mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`
 module.exports = {
     url : urlString,
-    init : function(){
-        
+    init : initialize,    
+    repopulate : function(){ 
         MongoClient.connect(urlString, (err, database) => {
-            if (err) return console.log(err);
-            database.collection("trucks")
-                .ensureIndex({loc:"2dsphere"}, (error, result) => {
-                    if (error)
-                        console.log(error);
+            if(err) console.log(err);
+            var d = new Date();
+            var todays = [];
+            console.log(d.getDay());
+            database.collection("trucks").drop(function(err, delOK) {
+                if (err) console.log(err);
+                if (delOK) {
+                    console.log("Truck Collection deleted");
+                    initialize();
+                }
+            });
+            database.collection('scheduledTrucks').find({}).toArray(function(err, docs){
+                docs.forEach(doc => {
+                    if(d.getDay() == doc.day){
+                        todays.push(doc);
+                        database.collection('trucks').insert(doc, (err, result) => {
+                            if(err){
+                                console.log({'error' : 'An error has occurred' })
+                            } else {
+                                console.log(result.ops[0]);
+                            }
+                        });
+                    }
                 });
-
+            });
         });
-    }
+    } 
+};
+
+function initialize(){
+    MongoClient.connect(urlString, (err, database) => {
+        if (err) return console.log(err);
+        database.collection("trucks")
+            .ensureIndex({loc:"2dsphere"}, (error, result) => {
+                if (error)
+                    console.log(error);
+            });
+
+    })
 };
--- repopulate.js
+++ repopulate.js
@@ -1,5 +1,3 @@
-const MongoClient         = require('mongodb');
-var schedule            = require("node-schedule");
 const db                  = require('./config/db');
 
 console.log("Starting repopulate script...")
@@ -7,32 +5,6 @@ console.log("Starting repopulate script...")
 var CronJob = require('cron').CronJob;
 //second, minute, hour, day
 var j = new CronJob('1 0 0 * * *', function() {
-    MongoClient.connect(db.url, (err, database) => {
-        if(err) console.log(err);
-        var d = new Date();
-        var todays = [];
-        console.log(d.getDay());
-        database.collection("trucks").drop(function(err, delOK) {
-            if (err) console.log(err);
-            if (delOK) {
-                console.log("Truck Collection deleted");
-                db.init();
-            }
-        });
-        database.collection('scheduledTrucks').find({}).toArray(function(err, docs){
-            docs.forEach(doc => {
-                if(d.getDay() == doc.day){
-                    todays.push(doc);
-                    database.collection('trucks').insert(doc, (err, result) => {
-                        if(err){
-                            console.log({'error' : 'An error has occurred' })
-                        } else {
-                            console.log(result.ops[0]);
-                        }
-                    });
-                }
-            });
-        });
-    });
+    db.repopulate();
 }, null, true, 'America/Boston');
 console.log(j.nextDates());
