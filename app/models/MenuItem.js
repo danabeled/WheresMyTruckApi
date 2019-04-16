1ca065fc2f0147c7fedf6af70db9c6deeca47ddf
Daniel Abel <danabeled@gmail.com>
2019-04-15 21:19:13 -0400

    add price field to menu item

--- app/models/MenuItem.js
+++ app/models/MenuItem.js
@@ -4,6 +4,10 @@ const MenuItemSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
+    },
+    price: {
+        type: Number,
+        required: true
     }
 })
 
--- spec/menuitem.spec.js
+++ spec/menuitem.spec.js
@@ -9,4 +9,22 @@ describe("Menu Item Schema", function(){
             expect(err.errors.name.message).toEqual("Path `name` is required.");
         })
     });
+
+    it("should require a price field", function(){
+        var m = new MenuItem();
+
+        m.validate(function(err){
+            expect(err.errors.price.message).toEqual("Path `price` is required.")
+        })
+    })
+
+    it("should not accept non-numbers for a price", function(){
+        var m = new MenuItem({
+            price: "Hello there!"
+        });
+
+        m.validate(function(err){
+            expect(err.errors.price.message).toEqual('Cast to Number failed for value "Hello there!" at path "price"');
+        })
+    })
 })
