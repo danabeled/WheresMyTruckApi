eec06b91c6bcf1a33eeb59bccb4e66065aae99e1
Daniel Abel <danabeled@gmail.com>
2019-04-17 19:22:24 -0400

    not allow dups

--- app/routes/api/menu.js
+++ app/routes/api/menu.js
@@ -1,9 +1,17 @@
 const router = require('express').Router();
 const Menu = require('../../models/Menu');
-router.post('/new', (req, res) => {
-    var menu = new Menu(req.body);
-    menu.save();
-    res.send({'message' : 'New Menu Added'});
+router.post('/new', (req, res) => 
+{
+    Menu.findOne({truck: req.body.truck}, function(err, result){
+        if(result == undefined){
+            var menu = new Menu(req.body);
+            menu.save();
+            res.send({'message' : 'New Menu Added'});
+        }
+        else{
+            res.status(400).send({'message' : 'Duplicate menu'});
+        }
+    });
 });
 
 module.exports = router;
